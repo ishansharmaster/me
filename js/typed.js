e6903c1371ac8bd740703f1433003484ba3bb10c
Ishan Sharma <Ishan Sharma>
2016-11-16 15:26:52 -0800

    sigstuff

--- js/typed.js
+++ js/typed.js
@@ -396,7 +396,7 @@
     };
 
     $.fn.typed.defaults = {
-        strings: ["These are the default values...", "You know what you should do?", "Use your own!", "Have a great day!"],
+        strings: ["My name is Ishan.", "I'm a student at Cal.", "Explore and enjoy!",],
         stringsElement: null,
         // typing speed
         typeSpeed: 0,
