6b231d7a47f09e593a6433063fd01049658276cf
Ajay Merchia <ajaymerchia@berkeley.edu>
2018-04-02 11:47:35 -0700

    ASUC Website V1 -- All Sponsors

--- js/main.js
+++ js/main.js
@@ -1,198 +1,138 @@
-(function($) {
-  "use strict";
-  $(window).on("load", function() { // makes sure the whole site is loaded
-    //preloader
-    $("#status").fadeOut(); // will first fade out the loading animation
-    $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
 
-    //masonry
-    $('.grid').masonry({
-      itemSelector: '.grid-item'
+$(document).ready(function(){
+	"use strict";
 
-    });
-  });
+	var window_width 	 = $(window).width(),
+	window_height 		 = window.innerHeight,
+	header_height 		 = $(".default-header").height(),
+	header_height_static = $(".site-header.static").outerHeight(),
+	fitscreen 			 = window_height - header_height;
 
 
-  $(document).ready(function(){
+	$(".fullscreen").css("height", window_height)
+	$(".fitscreen").css("height", fitscreen);
 
-    //active menu
-    $(document).on("scroll", onScroll);
-
-    $('a[href^="#"]').on('click', function (e) {
-      e.preventDefault();
-      $(document).off("scroll");
-
-      $('a').each(function () {
-        $(this).removeClass('active');
-      })
-      $(this).addClass('active');
-
-      var target = this.hash;
-      $target = $(target);
-      $('html, body').stop().animate({
-        'scrollTop': $target.offset().top+2
-      }, 500, 'swing', function () {
-        window.location.hash = target;
-        $(document).on("scroll", onScroll);
-      });
-    });
+   //-------- Active Sticky Js ----------//
+  $(".default-header").sticky({topSpacing:0});
 
 
-    //scroll js
-    smoothScroll.init({
-      selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
-      selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
-      speed: 500, // Integer. How fast to complete the scroll in milliseconds
-      easing: 'easeInOutCubic', // Easing pattern to use
-      updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
-      offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
-      callback: function ( toggle, anchor ) {} // Function to run after scrolling
-    });
+     if(document.getElementById("default-select")){
+          $('select').niceSelect();
+    };
 
-    //menu
-    var bodyEl = document.body,
-    content = document.querySelector( '.content-wrap' ),
-    openbtn = document.getElementById( 'open-button' ),
-    closebtn = document.getElementById( 'close-button' ),
-    isOpen = false;
-
-    function inits() {
-      initEvents();
-    }
-
-    function initEvents() {
-      openbtn.addEventListener( 'click', toggleMenu );
-      if( closebtn ) {
-        closebtn.addEventListener( 'click', toggleMenu );
-      }
-
-      // close the menu element if the target it´s not the menu element or one of its descendants..
-      content.addEventListener( 'click', function(ev) {
-        var target = ev.target;
-        if( isOpen && target !== openbtn ) {
-          toggleMenu();
+    $('.img-pop-up').magnificPopup({
+        type: 'image',
+        gallery:{
+        enabled:true
         }
-      } );
+    });
+
+    $('.play-btn').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+ 
+
+    // Select all links with hashes
+    $('.navbar-nav a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .on('click',function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top-50
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
     }
-
-    function toggleMenu() {
-      if( isOpen ) {
-        classie.remove( bodyEl, 'show-menu' );
-      }
-      else {
-        classie.add( bodyEl, 'show-menu' );
-      }
-      isOpen = !isOpen;
-    }
-
-    inits();
-
-
-    //typed js
-    $(".typed").typed({
-        strings: ["My name is Ishan.", "I'm a student at Cal.", "I'm also a:", "Brother.", "Developing Leader.", "Entrepreneur.", "Activist.", "Want to know more?", "Scroll down...", "Explore and enjoy!"],
-        typeSpeed: 10,
-        backDelay: 900,
-        // loop
-        loop: true
-    });
-
-    //owl carousel
-    $('.owl-carousel').owlCarousel({
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
-
-      items : 1,
-      itemsDesktop : [1199,1],
-      itemsDesktopSmall : [979,1],
-      itemsTablet : [768,1],
-      itemsMobile : [479,1],
-
-      // CSS Styles
-      baseClass : "owl-carousel",
-      theme : "owl-theme"
-    });
-
-    $('.owl-carousel2').owlCarousel({
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
-
-      items : 1,
-      itemsDesktop : [1199,1],
-      itemsDesktopSmall : [979,1],
-      itemsTablet : [768,1],
-      itemsMobile : [479,1],
-      autoPlay : false,
-
-      // CSS Styles
-      baseClass : "owl-carousel",
-      theme : "owl-theme"
-    });
-
-    //contact
-    $('input').blur(function() {
-
-      // check if the input has any value (if we've typed into it)
-      if ($(this).val())
-        $(this).addClass('used');
-      else
-        $(this).removeClass('used');
-    });
-
-    //pop up porfolio
-    $('.portfolio-image li a').magnificPopup({
-      type: 'image',
-      gallery: {
-        enabled: true
-      }
-      // other options
-    });
-
-    //Skill
-    jQuery('.skillbar').each(function() {
-      jQuery(this).appear(function() {
-        jQuery(this).find('.count-bar').animate({
-          width:jQuery(this).attr('data-percent')
-        },3000);
-        var percent = jQuery(this).attr('data-percent');
-        jQuery(this).find('.count').html('<span>' + percent + '</span>');
-      });
     });
 
 
+    $(document).ready(function() {
+
+    $('html, body').hide();
+
+        if (window.location.hash) {
+
+        setTimeout(function() {
+
+        $('html, body').scrollTop(0).show();
+
+        $('html, body').animate({
+
+        scrollTop: $(window.location.hash).offset().top
+
+        }, 1000)
+
+        }, 0);
+
+        }
+
+        else {
+
+        $('html, body').show();
+
+        }
+
+    });
+  
+
+
+    $(document).ready(function() {
+        $('#mc_embed_signup').find('form').ajaxChimp();
+    });      
+
+
+  $('.filters ul li').click(function(){
+    $('.filters ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    var data = $(this).attr('data-filter');
+    $grid.isotope({
+      filter: data
+    })
   });
 
 
-  //header
-  function inits() {
-    window.addEventListener('scroll', function(e){
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 300,
-            header = document.querySelector(".for-sticky");
-        if (distanceY > shrinkOn) {
-            classie.add(header,"opacity-nav");
-        } else {
-            if (classie.has(header,"opacity-nav")) {
-                classie.remove(header,"opacity-nav");
-            }
+  if(document.getElementById("portfolio")){
+        var $grid = $(".grid").isotope({
+          itemSelector: ".all",
+          percentPosition: true,
+          masonry: {
+            columnWidth: ".all"
           }
-      });
-    }
+        })
+  };
 
-  window.onload = inits();
 
-  //nav-active
-  function onScroll(event){
-    var scrollPosition = $(document).scrollTop();
-    $('.menu-list a').each(function () {
-      var currentLink = $(this);
-      var refElement = $(currentLink.attr("href"));
-      if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-        $('.menu-list a').removeClass("active");
-        currentLink.addClass("active");
-      }
-      else{
-        currentLink.removeClass("active");
-      }
-    });
-  }
 
-})(jQuery);
+
+
+
+ });
